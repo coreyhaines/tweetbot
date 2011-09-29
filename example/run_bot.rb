6c458a4237407ec1f15216df83970323bbe23277
coreyhaines <coreyhaines@gmail.com>
2011-09-29 17:35:09 -0500

    Example expansion

--- example/run_bot.rb
+++ example/run_bot.rb
@@ -4,8 +4,12 @@ require 'tweetbot'
 bot = TweetBot.configure do |config|
   config.response_frequency = 100
 
-  config.respond_to_phrase "#rubyconfbot" do |responses|
-    responses << "Enjoy RubyConf"
+  config.respond_to_phrase "tweetbot example phrase" do |responses|
+    responses << "I am tweetbot!" << "You rang?" << "Pretty cool, thanks for saying hello"
+  end
+
+  config.respond_to_phrase "hey @tweetbot" do |responses|
+    responses << "Hey back at ya" << "You rang again?"
   end
 
   config.twitter_auth = TwitterAuth::AuthKeys
